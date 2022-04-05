a6302c11fb42fd0fe65135a189fc783ac99f3ffb
Andre Santos <andre.santos.mail@gmail.com>
2022-04-05 16:32:08 +0100

    housekeeping

--- argocd/install-argocd.sh
+++
@@ -1,6 +0,0 @@
-kubectl create namespace argocd
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-
-# get password
-sleep 60
-kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
